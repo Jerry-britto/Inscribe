eee73fd9e4bd0bdd870a16acba9d60b1c0ab0560
Jerry-britto <jbritto2004@gmail.com>
2024-01-16 20:07:47 +0530

    fixed error in main

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:main/firebase_options.dart';
 
-void main() {
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() async{
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
