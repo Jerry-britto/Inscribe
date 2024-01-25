8a7af07688903b69315c8f66ae5cd1a8b9552b9f
Jerry-britto <jbritto2004@gmail.com>
2024-01-26 00:43:41 +0530

    errors resolved

--- main/lib/screens/signup.dart
+++ main/lib/screens/signup.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:main/screens/login/login_screen.dart';
 
@@ -10,11 +11,47 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   bool isVisible = true;
+  bool isVisible2 = true;
   final _formKey = GlobalKey<FormState>();
   final email = TextEditingController();
   final password = TextEditingController();
   final cnfpassword = TextEditingController();
   bool _isToggled = false;
+
+  void displayMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text(message),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Ok"))
+        ],
+      ),
+    );
+  }
+
+  void registerUser(String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((value) {
+        // Navigate user to home
+        print("User signed up");
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        displayMessage('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        displayMessage('The account already exists for that email.');
+      } else {
+        displayMessage(e.message.toString());
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -56,11 +93,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       )
                     ],
                   ),
-
                   const SizedBox(
                     height: 20,
                   ),
-
                   ToggleButtons(
                     onPressed: (int index) {
                       setState(() {
@@ -69,20 +104,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     },
                     borderRadius: const BorderRadius.all(Radius.circular(50)),
                     borderColor: Colors.black,
-                    selectedColor:const Color.fromRGBO(162, 7, 48, 1),
+                    selectedColor: const Color.fromRGBO(162, 7, 48, 1),
                     selectedBorderColor: const Color.fromRGBO(162, 7, 48, 1),
-                    constraints: const BoxConstraints(minWidth: 100, minHeight: 50),
+                    constraints:
+                        const BoxConstraints(minWidth: 100, minHeight: 50),
                     isSelected: [_isToggled, !_isToggled],
                     children: const [
                       Text("SWD"),
                       Text("Scribe"),
                     ],
                   ),
-
                   const SizedBox(
                     height: 20,
                   ),
-
                   TextFormField(
                     controller: email,
                     validator: (value) {
@@ -100,22 +134,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       filled: true,
                       fillColor: Colors.white,
                       enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
-                          borderSide: BorderSide(color: Color.fromRGBO(162, 7, 48, 1)), 
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
-                          borderSide: BorderSide(color: Color.fromRGBO(162, 7, 48, 1)), 
-                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide:
+                            BorderSide(color: Color.fromRGBO(162, 7, 48, 1)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide:
+                            BorderSide(color: Color.fromRGBO(162, 7, 48, 1)),
+                      ),
                       hintText: "Enter Email",
                       prefixIcon: Icon(Icons.mail),
                     ),
                   ),
-
                   const SizedBox(
                     height: 25,
                   ),
-
                   TextFormField(
                     controller: password,
                     validator: (val) {
@@ -129,11 +163,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         fillColor: Colors.white,
                         enabledBorder: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                          borderSide: BorderSide(color: Color.fromRGBO(162, 7, 48, 1)), 
+                          borderSide:
+                              BorderSide(color: Color.fromRGBO(162, 7, 48, 1)),
                         ),
                         focusedBorder: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                          borderSide: BorderSide(color: Color.fromRGBO(162, 7, 48, 1)), 
+                          borderSide:
+                              BorderSide(color: Color.fromRGBO(162, 7, 48, 1)),
                         ),
                         hintText: "Enter Password",
                         prefixIcon: const Icon(Icons.lock),
@@ -146,19 +182,15 @@ class _SignUpScreenState extends State<SignUpScreen> {
                             icon: const Icon(Icons.remove_red_eye_sharp))),
                     obscureText: isVisible,
                   ),
-
                   const SizedBox(
                     height: 25,
                   ),
-
                   TextFormField(
                     controller: cnfpassword,
                     validator: (val) {
                       if (val == null || val.isEmpty) {
                         return "Kindly enter your Password";
-                      }
-                      else if(val!=password.text)
-                      {
+                      } else if (val != password.text) {
                         return "This password doesn't match the previously entered password";
                       }
                       return null;
@@ -168,76 +200,47 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         fillColor: Colors.white,
                         enabledBorder: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                          borderSide: BorderSide(color: Color.fromRGBO(162, 7, 48, 1)), 
+                          borderSide:
+                              BorderSide(color: Color.fromRGBO(162, 7, 48, 1)),
                         ),
                         focusedBorder: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                          borderSide: BorderSide(color: Color.fromRGBO(162, 7, 48, 1)), 
+                          borderSide:
+                              BorderSide(color: Color.fromRGBO(162, 7, 48, 1)),
                         ),
                         hintText: "Re-enter Password",
                         prefixIcon: const Icon(Icons.lock),
                         suffixIcon: IconButton(
                             onPressed: () {
                               setState(() {
-                                isVisible = !isVisible;
+                                isVisible2 = !isVisible2;
                               });
                             },
                             icon: const Icon(Icons.remove_red_eye_sharp))),
-                    obscureText: isVisible,
+                    obscureText: isVisible2,
                   ),
-
                   const SizedBox(
                     height: 30,
                   ),
-
                   SizedBox(
                     width: double.infinity,
                     child: FloatingActionButton(
-                      backgroundColor: const Color.fromRGBO(162, 7, 48, 1),
-                      onPressed: () {
-                        if(_formKey.currentState!.validate()){
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Processing Data')),
-                          );
-                        }
-                      },
-                      child: const Text(
-                        "Register",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      )
-                    ),
+                        backgroundColor: const Color.fromRGBO(162, 7, 48, 1),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            registerUser(email.text, cnfpassword.text);
+                          }
+                        },
+                        child: const Text(
+                          "Register",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
                   ),
-
                   const SizedBox(
                     height: 24,
                   ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 20,
-                        child: Image.asset("assets/images/google_icon.webp"),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-
-                      TextButton(
-                        child: const Text(
-                          "Sign Up with Google",
-                          style: TextStyle(color: Colors.blue,),
-                        ),
-                        onPressed: () {
-                          print("Sign Up with Google");
-                        },
-                      ),
-
-                    ],
-                  ),
-
                   const SizedBox(
-                    height: 15,
+                    height: 5,
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -246,11 +249,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         "Already have an account ?",
                         style: TextStyle(fontSize: 16),
                       ),
-
                       const SizedBox(
                         width: 0,
                       ),
-
                       TextButton(
                           onPressed: () {
                             print("go to sign in");
